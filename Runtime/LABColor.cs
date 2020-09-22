827d5fa8b84cd9c9c10b34dcb4dbc0cf6c42bab4
Vorontcov Gleb <vorontcovg@taktikagames.ru>
2020-09-22 18:43:44 +0300

    replace virtual texture, added color extensions

---
+++ Runtime/LABColor.cs
@@ -0,0 +1,44 @@
+﻿namespace UniModules.UniGame.GraphicsTools.Runtime
+{
+    using UnityEngine;
+
+    public readonly struct LABColor
+    {
+        public float L { get; }
+        public float A { get; }
+        public float B { get; }
+
+        public LABColor(float l, float a, float b)
+        {
+            L = l;
+            A = a;
+            B = b;
+        }
+        
+        public static LABColor ToLABColor(Color color)
+        {
+            var xyz   = TristimulusColor.ToTristimulusColor(color);
+            var white = TristimulusColor.White;
+
+            var x = PivotXYZ(xyz.X / white.X);
+            var y = PivotXYZ(xyz.Y / white.Y);
+            var z = PivotXYZ(xyz.Z / white.Z);
+
+            var l = Mathf.Max(0.0f, 116.0f * y - 16.0f);
+            var a = 500.0f * (x - y);
+            var b = 200.0f * (y - z);
+                
+            return new LABColor(l, a, b);
+        }
+
+        private static float PivotXYZ(float n)
+        {
+            return n > TristimulusColor.Epsilon ? CubicRoot(n) : (TristimulusColor.Kappa * n + 16.0f) / 116.0f;
+        }
+
+        private static float CubicRoot(float n)
+        {
+            return Mathf.Pow(n, 1.0f / 3.0f);
+        }
+    }
+}
---
+++ Runtime/TristimulusColor.cs
@@ -0,0 +1,41 @@
+﻿namespace UniModules.UniGame.GraphicsTools.Runtime
+{
+    using UnityEngine;
+
+    public readonly struct TristimulusColor
+    {
+        public static TristimulusColor White => new TristimulusColor(95.047f, 100.000f, 108.883f);
+        
+        public const float Epsilon = 0.008856f;
+        public const float Kappa   = 903.3f;
+        
+        public float X { get; }
+        public float Y { get; }
+        public float Z { get; }
+
+        public TristimulusColor(float x, float y, float z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+        
+        public static TristimulusColor ToTristimulusColor(Color color)
+        {
+            var r = PivotRgb(color.r);
+            var g = PivotRgb(color.g);
+            var b = PivotRgb(color.b);
+
+            var x = r * 0.4124f + g * 0.3576f + b * 0.1805f;
+            var y = r * 0.2126f + g * 0.7152f + b * 0.0722f;
+            var z = r * 0.0193f + g * 0.1192f + b * 0.9505f;
+                
+            return new TristimulusColor(x, y, z);
+        }
+
+        private static float PivotRgb(float n)
+        {
+            return (n > 0.04045f ? Mathf.Pow((n + 0.055f) / 1.055f, 2.4f) : n / 12.92f) * 100.0f;
+        }
+    }
+}
